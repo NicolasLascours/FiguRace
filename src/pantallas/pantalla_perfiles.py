bd98a9d862ce7197e43e2f4b5e518ecbcff27008
Lautaro Fuente <lautaro.fuente@yahoo.com>
2022-05-23 03:40:03 -0300

    Mejoro pantalla perfiles

--- src/pantallas/pantalla_perfiles.py
+++ src/pantallas/pantalla_perfiles.py
@@ -3,6 +3,7 @@ from .layouts.layout_perfiles import layout_perfiles
 from .handlers.handler_perfiles import handler_perfiles
 import PySimpleGUI as sg
 
+
 def ventana_perfiles():
     # creo ventana perfiles
     sg.theme('LightBlue4')
@@ -13,7 +14,9 @@ def ventana_perfiles():
         if event == sg.WIN_CLOSED or event == "Volver":
             break
         else:
+            window.Hide()
             # llamo al modulo handler pasandole el evento que se produjo
             handler_perfiles(event)
+            window.UnHide()
     # cierro la ventana
     window.close()
