edc9c94d91f54ed83b6026e57a13edaade3ed23a
franviscardi <franviscardi@hotmail.com>
2022-05-24 20:39:24 -0300

    cambio puntajes

---
+++ grupo18/src/pantallas/pantalla_puntajes.py
@@ -0,0 +1,15 @@
+# importo modulos a usar
+from .layouts.layout_puntajes import layout_puntajes
+import PySimpleGUI as sg
+
+def ventana_puntajes():
+    # creo ventana puntajes
+    sg.theme('LightBlue4')
+    window = sg.Window("Puntajes", layout_puntajes(), margins=(200, 150))
+    # creo el bucle
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Volver":
+            break
+    # cierro la ventana
+    window.close()
