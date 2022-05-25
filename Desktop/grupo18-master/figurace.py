50a2d0646625011a595402a28ecb141e00635f16
franviscardi <franviscardi@hotmail.com>
2022-05-24 21:32:34 -0300

    Eliminacion de carpeta

--- Desktop/grupo18-master/figurace.py
+++
@@ -1,5 +0,0 @@
-# PROGRAMA PRINCIPAL
-from src.pantallas.pantalla_principal import start
-
-if __name__ == "__main__":
-    start()
--- Desktop/grupo18-master/src/layouts/pantallas/configuracion/__init__.py
+++
@@ -1,3 +0,0 @@
-from .configuracion_pantalla import ventana_configuracion
-
-__all__ = ['ventana_configuracion']
