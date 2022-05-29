9f06b93e1cfb6c5cc138b655c40fff0880f530b1
Franco,Viscardi Robles <franviscardi@hotmail.com>
2022-05-29 20:21:42 -0300

    Update pantalla_juego.py

--- src/pantallas/pantalla_juego.py
+++ src/pantallas/pantalla_juego.py
@@ -1,11 +1,10 @@
 import PySimpleGUI as sg
 from .layouts.layout_juego import layouts
-
+from .handlers.handler_juego import eventos
 
 # aca va para poder abrir el archivo de los dataset para el juego
 def archivo_dataset():
     pass
-
 
 def comenzar():
     """
@@ -13,13 +12,16 @@ def comenzar():
     """
     lista = layouts()
     puntaje = 0
+    ronda_act = 1
     # ventana del juego
-    ventana = sg.Window('Figurace', lista, size=(500, 300))
-    while True:
+    ventana = sg.Window('Figurace', lista, size=(500, 500))
+    while True and ronda_act <= 10:
         evento = ventana.read()
-        if evento[0] == sg.WIN_CLOSED or evento[0] == "Volver a la pantalla principal":
+        ronda_act = eventos (evento[0], ronda_act)
+        if evento[0] == sg.WIN_CLOSED or evento[0] == "Abandonar el juego":
             break
+        ventana.refresh()
     sg.popup_ok('La cantidad de puntos obtenidos es: ', puntaje)
     ventana.close()
 
-# abrir_archivo()
+#abrir_archivo()
