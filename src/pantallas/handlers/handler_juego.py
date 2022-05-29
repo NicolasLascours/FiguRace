9a575fe3fc3f18e083c55caae91d6c44c918f8c9
Franco,Viscardi Robles <franviscardi@hotmail.com>
2022-05-29 20:21:02 -0300

    Update handler_juego.py

--- src/pantallas/handlers/handler_juego.py
+++ src/pantallas/handlers/handler_juego.py
@@ -1,8 +1,7 @@
 import os
 import json
 
-
-def abrir_configuracion():
+def abrir_configuracion ():
     """
     funcion para poder abrir el archivo json
     de las configuraciones del juego
@@ -10,4 +9,21 @@ def abrir_configuracion():
     ruta_archivo = os.path.join('config.json')
     archivo_json = open(ruta_archivo, 'r')
     datos = json.load(archivo_json)
+    archivo_json.close()
     return datos
+
+def abrir_dificultad ():
+    """
+    Funcion para poder abrir el archivo json
+    de la dificultad del juego
+    """
+    ruta_archivo = os.path.join('dificultad.json')
+    archivo_json = open(ruta_archivo, 'r')
+    datos = json.load(archivo_json)
+    archivo_json.close()
+    return datos
+
+def eventos(evento, ronda_act):
+    if evento == 'Pasar':
+        ronda_act += 1
+    return ronda_act
