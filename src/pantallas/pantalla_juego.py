c2064cd33127bc65469d8db3cc8a87d4a815fcd8
Lautaro Fuente <lautaro.fuente@yahoo.com>
2022-06-23 16:47:10 -0300

    funcion para que se guarden las partidas en csv puntajes

--- src/pantallas/pantalla_juego.py
+++ src/pantallas/pantalla_juego.py
@@ -4,6 +4,18 @@ from src.objects.partida import Partida
 import random
 from .layouts.layout_juego import layouts, config_inicial
 from .handlers.handler_juego import abrir_configuracion, actualizar_tiempo, eventos, registro_jugadas, actualizacion
+from roots import ROOT_PUNTAJES
+from .handlers.handler_perfiles import datos_de_perfiles, obtener_datos_nick
+import csv
+
+
+def guardar_partida(perfil_actual,estado,puntaje,dificultad):
+    datos = datos_de_perfiles()
+    edad, genero = obtener_datos_nick(perfil_actual, datos)
+    with open (ROOT_PUNTAJES, 'a', encoding='utf_8') as archivo:
+        writer = csv.writer(archivo)
+        writer.writerow([perfil_actual, genero, estado, puntaje, dificultad])
+
 
 def act_completa(ventana, cant, header, datos):
     lista_data = []
@@ -30,6 +42,7 @@ def comenzar(ROOT_DIR, perfil_actual, data):
             correcta, lista_data, lista_carac = act_completa(ventana, cant, header, datos)
         if evento[0] == sg.WIN_CLOSED or evento[0] == "Abandonar el juego":
             registro_jugadas(evento[0], ROOT_DIR, perfil_actual, correcta, lista_data, '')
+            guardar_partida(perfil_actual,"Cancelada",partida.puntaje(),config["Dificultad"])
             break
         restante = actualizar_tiempo(ventana,partida)
         if evento[0] == "__TIMEOUT__" and restante <= 0:
@@ -38,4 +51,5 @@ def comenzar(ROOT_DIR, perfil_actual, data):
     sg.popup_ok('La cantidad de puntos obtenidos es: ', partida.puntaje())
     if evento[0] != "Abandonar el juego" and evento[0] != sg.WIN_CLOSED:
         registro_jugadas(evento[0], ROOT_DIR, perfil_actual, correcta, lista_data, '')
+        guardar_partida(perfil_actual,"Finalizada",partida.puntaje(),config["Dificultad"])
     ventana.close()
