016602a44ffb5a4e7e191f2335494d5f930fb2c7
Franco,Viscardi Robles <franviscardi@hotmail.com>
2022-06-24 05:37:36 -0300

    Update handler_juego.py

--- src/pantallas/handlers/handler_juego.py
+++ src/pantallas/handlers/handler_juego.py
@@ -1,8 +1,9 @@
 import json
 import PySimpleGUI as sg
-from roots import ROOT_REGISTRO, ROOT_VOLCANS, ROOT_LAGOS, ROOT_FIFA, ROOT_CONFIG
+from roots import ROOT_REGISTRO, ROOT_VOLCANS, ROOT_LAGOS, ROOT_FIFA, ROOT_CONFIG, ROOT_PERFILES
 import csv
 import time
+import os
 
 def abrir_volcanes():
     """
@@ -62,33 +63,33 @@ def abrir_configuracion():
             return dif_por_defecto    
 
 
-def inicializacion_partida (perfil_actual, dif, uui):
-    with open (ROOT_REGISTRO, 'a') as reg:
-        writer = csv.writer(reg)
-        writer.writerow([time.time(), uui, "inicio_partida",
-                        perfil_actual, '', '', '', dif])
-
-
-def registro_jugadas (evento, perfil_actual, correcta, lista_data, respuesta, uui):
+ ##["Timestamp", "ID", "Objectos a adivinar", "genero" ,"Evento", "Usuario", "Estado", "Texto Ingresado", "Respuesta", "Nivel"]
+def registro_jugadas (evento, perfil_actual, correcta, lista_data, respuesta, uui, partida):
     """
     Funcion que actualiza los eventos que ocurren en
     el juego y los guarda en un archico csv
     """
     config = abrir_configuracion()
+    with open (ROOT_PERFILES, 'r') as reg:
+        perfiles = json.load(reg)
+        i = 0
+        while (i < len(perfiles) and (perfiles[i]['nick'] != perfil_actual)):
+            i+=1
+        gen = perfiles[i]['genero']
     with open (ROOT_REGISTRO, 'a', encoding='utf_8') as reg:
         writer = csv.writer(reg)
         if (evento == '__TIMEOUT__'):
-            writer.writerow([time.time(), uui, 'intento', perfil_actual, "timeout", "", correcta[5], config["Dificultad"]])
+            writer.writerow([time.time(), uui, partida.cant_rondas , gen ,'Intento', perfil_actual, "timeout", "", correcta[5], config["Dificultad"]])
         elif (evento == "Correcta"):
-            writer.writerow([time.time(), uui, 'intento', perfil_actual, "ok", lista_data[respuesta][5], correcta[5], config["Dificultad"]])
+            writer.writerow([time.time(), uui, partida.cant_rondas , gen ,'Intento', perfil_actual, "ok", lista_data[respuesta][5], correcta[5], config["Dificultad"]])
         elif (evento == "Pasar"):
-            writer.writerow([time.time(), uui, 'omitir', perfil_actual, "omision", '' , correcta[5], config["Dificultad"]])
+            writer.writerow([time.time(), uui, partida.cant_rondas , gen ,'Omitir', perfil_actual, "Omision", '' , correcta[5], config["Dificultad"]])
         elif (evento == "Incorrecta"):
-            writer.writerow([time.time(), uui, 'intento', perfil_actual, "error", lista_data[respuesta][5], correcta[5], config["Dificultad"]])
+            writer.writerow([time.time(), uui, partida.cant_rondas , gen ,'Intento', perfil_actual, "error", lista_data[respuesta][5], correcta[5], config["Dificultad"]])
         elif (evento == "Abandonar el juego" or evento == sg.WIN_CLOSED):
-            writer.writerow([time.time(), uui, 'fin', perfil_actual, "cancelada", '', '', config["Dificultad"]])
+            writer.writerow([time.time(), uui, partida.cant_rondas , gen ,'fin', perfil_actual, "cancelada", '', '', config["Dificultad"]])
         else:
-            writer.writerow([time.time(), uui, 'fin', perfil_actual, "finalizada", '', '', config["Dificultad"]])
+            writer.writerow([time.time(), uui, partida.cant_rondas , gen,'fin', perfil_actual, "finalizada", '', '', config["Dificultad"]])
 
 
 def convert(seconds):
@@ -145,15 +146,15 @@ def eventos(evento, ventana, partida, correcta, lista_data, perfil_actual, uui):
         if partida.ronda_actual <= partida.cant_rondas:
             actualizar_partida(ventana, partida)
             ventana["-Puntaje-"].update(f'Puntaje: {partida.puntaje()}')
-            registro_jugadas(evento, perfil_actual, correcta, lista_data, '', uui)
+            registro_jugadas(evento, perfil_actual, correcta, lista_data, '', uui, partida)
     elif (evento == 'OPCION 0' or evento == 'OPCION 1' or evento == 'OPCION 2' 
             or evento == 'OPCION 3' or evento == 'OPCION 4'): 
             respuesta = int(evento[-1])
             if lista_data[respuesta][5] == correcta[5]:
                 partida.incrementar_puntaje()
-                registro_jugadas("Correcta", perfil_actual, correcta, lista_data, respuesta, uui)
+                registro_jugadas("Correcta", perfil_actual, correcta, lista_data, respuesta, uui, partida)
             else:
                 partida.decrementar_puntaje()
-                registro_jugadas("Incorrecta", perfil_actual, correcta, lista_data, respuesta, uui)
+                registro_jugadas("Incorrecta", perfil_actual, correcta, lista_data, respuesta, uui, partida)
             ventana['-Puntaje-'].update(f'Puntaje: {partida.puntaje()}')
             actualizar_partida(ventana, partida)
